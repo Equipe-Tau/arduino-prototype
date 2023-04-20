8b32153d559e35365be8c3d0af48cebea5c89646
LearXD <50178057+LearXD@users.noreply.github.com>
2023-04-19 22:16:54 -0300

    Update all.ino

--- all.ino
+++ all.ino
@@ -7,209 +7,191 @@
 
 #define SCHEDULE_SIZE 10
 
-#define WIFI_SSID "WilliameLucia"
-#define WIFI_PASSWORD "@Lucia1981"
+#define WIFI_SSID "IFMG_Servidores"
+#define WIFI_PASSWORD NULL
 
 #define WIFI_LED 2
 #define RED_LED 18
 #define GREEN_LED 22
 
 #define LOCK_SERVO_PORT 26
+#define LOCK_SERVO_OPENED 130
+#define LOCK_SERVO_CLOSED 0
 
 #define PUSH_BUTTON 15
 
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial2);
 Servo lockServo;
 
-typedef struct
-{
-    int port;
-    int time;
-    int state;
-} Schedule;
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Inicializando"));
 
-void setup()
-{
-    Serial.begin(115200);
-    Serial.println(F("Inicializando"));
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(WIFI_LED, OUTPUT);
 
-    pinMode(RED_LED, OUTPUT);
-    pinMode(GREEN_LED, OUTPUT);
-    pinMode(WIFI_LED, OUTPUT);
+  setupWifi(WIFI_SSID, WIFI_PASSWORD);
 
-    Serial.println(F("Iniciando Wi-Fi..."));
-    setupWifi(WIFI_SSID, WIFI_PASSWORD);
+  sendRequest("Iniciando...", true, 1);
 
-    sendRequest(getName(0), true, 1);
+  Serial.println(F("Iniciando sensor de impressão digital..."));
+  finger.begin(57600);
+  if (finger.verifyPassword()) {
+    Serial.println(F("Sensor de impressão digital encontrado!"));
+  } else {
+    Serial.println(F("Não foi possível encontrar o sensor de impressão digital :("));
+    return;
+  }
 
-    Serial.println(F("Iniciando sensor de impressão digital..."));
-    finger.begin(57600);
-    if (finger.verifyPassword())
-    {
-        Serial.println(F("Sensor de impressão digital encontrado!"));
-    }
-    else
-    {
-        Serial.println(F("Não foi possível encontrar o sensor de impressão digital :("));
-        return;
-    }
-
-    Serial.println(F("Iniciando servo..."));
-    lockServo.attach(LOCK_SERVO_PORT);
-    moveServo(lockServo, 130);
+  Serial.println(F("Iniciando servo..."));
+  lockServo.attach(LOCK_SERVO_PORT);
+  moveServo(lockServo, LOCK_SERVO_OPENED);
 }
 
-void loop()
-{
-    wifiManager();
-    fingerManager();
-    schedule();
-    delay(10);
+void loop() {
+  wifiManager();
+  fingerManager();
+  schedule();
+  delay(10); // TALVEZ AUMENTAR ISSO SEJA INTERESSANTE
 }
 
-String getName(int number)
-{
-    String nomes[] = {"Migue%20Pinheiro", "Miguel%20BOB", "Raissa", "Roniery", "Leandro", "Gustavo", "Julia"};
-    if(number > ((sizeof(nomes) / sizeof(nomes[0])) - 1) || number < 0) {
-      return "Nome não definido";
-    }
-    return nomes[number];
+String getName(int number) {
+  String nomes[] = { "Migue%20Pinheiro", "Miguel%20BOB", "Raissa", "Roniery", "Leandro", "Gustavo", "Julia" };
+  if (number > ((sizeof(nomes) / sizeof(nomes[0])) - 1) || number < 0) {
+    return "Nome não definido";
+  }
+  return nomes[number];
 }
 
-void setupWifi(char *name, char *password)
-{
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(name, password);
+void setupWifi(char *name, char *password) {
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(100);
-        Serial.print(".");
-    }
+  Serial.println(F("Conectando ao Wi-Fi..."));
 
-    digitalWrite(WIFI_LED, HIGH);
-    Serial.println("\nConectado!");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(name, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  digitalWrite(WIFI_LED, HIGH);
+  Serial.println(F("\nConectado!"));
 }
 
-void wifiManager()
-{
-    switch (WiFi.status())
-    {
+void wifiManager() {
+  switch (WiFi.status()) {
     case WL_CONNECTED:
-        digitalWrite(WIFI_LED, HIGH);
-        break;
+      digitalWrite(WIFI_LED, HIGH);
+      break;
     case WL_NO_SSID_AVAIL:
-        Serial.println(F("Rede não encontrada"));
-        break;
+      Serial.println(F("Rede não encontrada"));
+      break;
     case WL_CONNECT_FAILED:
     case WL_CONNECTION_LOST:
     case WL_DISCONNECTED:
-        digitalWrite(WIFI_LED, LOW);
-        Serial.println(F("Reconectando..."));
-        WiFi.reconnect();
-        break;
+      digitalWrite(WIFI_LED, LOW);
+      Serial.println(F("Reconectando..."));
+      WiFi.reconnect();
+      break;
     default:
-        Serial.println(F("Rede ociosa..."));
-        break;
-    }
+      Serial.println(F("Rede ociosa..."));
+      break;
+  }
 }
 
-void fingerManager()
-{
-    Serial.print(".");
-    int result = finger.getImage();
-    if (result == FINGERPRINT_OK)
-    {
-        Serial.println(F("Nova ocorrencia de dedo!"));
-        if (finger.image2Tz() != FINGERPRINT_OK)
-        {
-            Serial.println(F("Erro na conversão da imagem!"));
-            return;
-        }
+void fingerManager() {
+  Serial.print(F("*"));
 
-        if (finger.fingerFastSearch() != FINGERPRINT_OK)
-        {
-            Serial.println(F("Dedo não encontrado!"));
-            digitalWrite(RED_LED, HIGH);
-            addSchedule(RED_LED, LOW, 5);
-            return;
-        }
-
-        digitalWrite(GREEN_LED, HIGH);
-        addSchedule(GREEN_LED,LOW,5);
-        lockServo.write(0);
-        
-        sendRequest(getName(finger.fingerID), true /* VERIFICAR PUSH BUTTON */, 308);
-        Serial.println("Digital de " + getName(finger.fingerID) + " encontrada!");
-        
-    return;        
+  int result = finger.getImage();
+  if (result == FINGERPRINT_OK) {
+    Serial.println(F("Nova ocorrencia de dedo!"));
+    if (finger.image2Tz() != FINGERPRINT_OK) {
+      Serial.println(F("Erro na conversão da imagem!"));
+      return;
     }
-    
+
+    if (finger.fingerFastSearch() != FINGERPRINT_OK) {
+      Serial.println(F("Dedo não encontrado!"));
+      digitalWrite(RED_LED, HIGH);
+      addSchedule(RED_LED, LOW, 5);
+      return;
+    }
+
+    digitalWrite(GREEN_LED, HIGH);
+    addSchedule(GREEN_LED, LOW, 5);
+    lockServo.write(LOCK_SERVO_CLOSED);
+
+    sendRequest(getName(finger.fingerID), true /* VERIFICAR PUSH BUTTON */, 308);
+    Serial.println("Digital de " + getName(finger.fingerID) + " encontrada!");
+
+    return;
+  }
 }
 
-bool sendRequest(String name, bool took, int port)
-{
-    HTTPClient http;
+bool sendRequest(String name, bool took, int port) {
+  HTTPClient http;
 
-    http.begin("https://docs.google.com/forms/d/e/1FAIpQLSczsF3e2sIlJW1pv5M4xh0arTnAgQu9jG1RlhVZYEeMOv-adA/formResponse?ifq&entry.669983157=" + name + "&entry.1164638721=" + (took ? "Pegou" : "Guardou") + "%20a%20chave%20" + String(port) + "&submit=Submit");
-    //Serial.println("https://docs.google.com/forms/d/e/1FAIpQLSczsF3e2sIlJW1pv5M4xh0arTnAgQu9jG1RlhVZYEeMOv-adA/formResponse?ifq&entry.669983157=" + name + "&entry.1164638721=" + (took ? "Pegou" : "Guardou") + "%20a%20chave%20" + String(port) + "&submit=Submit");
+  http.begin("https://docs.google.com/forms/d/e/1FAIpQLSczsF3e2sIlJW1pv5M4xh0arTnAgQu9jG1RlhVZYEeMOv-adA/formResponse?ifq&entry.669983157=" + name + "&entry.1164638721=" + (took ? "Pegou" : "Guardou") + "%20a%20chave%20" + String(port) + "&submit=Submit");
 
-    int httpCode = http.GET();
+  int httpCode = http.GET();
 
-    if (httpCode <= 0)
-    {
-        Serial.printf("[HTTP] GET request failed, error: %s\n", http.errorToString(httpCode).c_str());
-        return false;
-    }
-    Serial.printf("[HTTP] GET status code: %d\n", httpCode);
-    http.end();
-    return true;
+  Serial.printf("[HTTP] Status Code: %d\n", httpCode);
+
+  if (httpCode <= 0) {
+    Serial.printf("[HTTP] A requisição GET falou, erro: %s\n", http.errorToString(httpCode).c_str());
+  } 
+  
+  http.end();
+  return true;
 }
 
-void moveServo(Servo servo, int target)
-{
-    int current = servo.read();
-    int increase = current < target;
-    for (int i = current; (increase ? (i <= target) : (i >= target)); (increase ? i++ : i--))
-    {
-        servo.write(i);
-    }
+
+
+void moveServo(Servo servo, int target) {
+  int current = servo.read();
+  int increase = current < target;
+  for (int i = current; (increase ? (i <= target) : (i >= target)); (increase ? i++ : i--)) {
+    servo.write(i);
+  }
 }
 
 /*
  * PARTE DE SCHEDULE (AGENDAMENTO DE TAREFAS)
  */
 
+typedef struct
+{
+  int port;
+  int time;
+  int state;
+} Schedule;
+
 Schedule schedules[SCHEDULE_SIZE];
 
-void schedule()
-{
-    int now = millis();
-    for (int i = 0; i < (sizeof(schedules) / sizeof(schedules[0])); i++)
-    {
-        Schedule currentSchedule = schedules[i];
-        if ((currentSchedule.time - now) <= 0)
-        {
-            digitalWrite(currentSchedule.port, currentSchedule.state);
-            schedules[i] = {0, 0, 0};
-        }
+void schedule() {
+  int now = millis();
+  for (int i = 0; i < (sizeof(schedules) / sizeof(schedules[0])); i++) {
+    Schedule currentSchedule = schedules[i];
+    if ((currentSchedule.time - now) <= 0) {
+      digitalWrite(currentSchedule.port, currentSchedule.state);
+      schedules[i] = { 0, 0, 0 };
     }
+  }
 }
 
-bool addSchedule(int port, int state, int toSeconds)
-{
-    int now = millis();
-    Schedule newSchedule = {port, now + (toSeconds * 1000), state};
+bool addSchedule(int port, int state, int toSeconds) {
+  int now = millis();
+  Schedule newSchedule = { port, now + (toSeconds * 1000), state };
 
-    for (int i = 0; i < (sizeof(schedules) / sizeof(schedules[0])); i++)
-    {
-        Schedule currentSchedule = schedules[i];
-        if ((currentSchedule.time - now) <= 0)
-        {
-            schedules[i] = newSchedule;
-        }
-        return true;
+  for (int i = 0; i < (sizeof(schedules) / sizeof(schedules[0])); i++) {
+    Schedule currentSchedule = schedules[i];
+    if ((currentSchedule.time - now) <= 0) {
+      schedules[i] = newSchedule;
     }
+    return true;
+  }
 
-    return false;
+  return false;
 }
